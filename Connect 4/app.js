f880793a0c20117baf89af9cea33cc19d91cec2d
DalilahH1 <dalilahheyward@gmail.com>
2025-04-14 16:59:19 -0400

    game update
    adds diagonal 4 match acknowledgment

--- Connect 4/app.js
+++ Connect 4/app.js
@@ -15,7 +15,10 @@ $(document).ready(function(){
             if(isValid($(this).attr("id"))){
                 $(this).css("background-color", colors[player]);
                 $(this).attr("data-player", player);
-
+                if(checkWin(player)){
+                    alert(colors[player] + " has won!");
+                    winner = player;
+                }
                 player *= -1;
             }
         });
@@ -23,7 +26,9 @@ $(document).ready(function(){
 
     function isValid(n){
         var id = parseInt(n);
-
+        if(winner !== 0){
+            return false;
+        }
         if($("#" + id).attr("data-player") === "0"){
             if(id >= 35){
                 return true;
@@ -33,6 +38,74 @@ $(document).ready(function(){
             }
         }
 
-        return false
+        return false;
+    }
+
+    function checkWin(p){
+        //check rows
+        var chain = 0;
+        for(var i = onabort; i < 42; i+=7){
+            for(var j = 0; j < 7; j++){
+                var cell = $("#" + (i+j));
+                if(cell.attr("data-player") == p){
+                    chain++;
+                }else{
+                    chain=0;
+                }
+
+                if(chain >= 4){
+                    return true;
+                }
+
+            }
+            chain = 0;
+        }
+
+        //check columns
+        chain = 0;
+        for(var i = 0; i < 7; i++){
+            for(var j = 0; j < 42; j+=7){
+                var cell = $("#" + (i + j));
+                if(cell.attr("data-player") == p){
+                    chain++;
+                }else{
+                    chain = 0;
+                }
+
+                if(chain >= 4){
+                    return true;
+                }
+            }
+            chain = 0;
+        }
+
+        //check diagnoals
+        var topLeft = 0;
+        var topRight = topLeft + 3;
+
+        for(var i = 0; i < 3; i++){
+            for(var j = 0; j < 4; j++){
+                if($("#" + topLeft).attr("data-player") == p
+                && $("#" + (topLeft + 8)).attr("data-player") == p
+                && $("#" + (topLeft + 16)).attr("data-player") == p
+                && $("#" + (topLeft + 24)).attr("data-player") == p){
+                    return true;
+                }
+
+                if($("#" + topRight).attr("data-player") == p
+                && $("#" + (topRight + 6)).attr("data-player") == p
+                && $("#" + (topRight + 12)).attr("data-player") == p
+                && $("#" + (topRight + 18)).attr("data-player") == p){
+                    return true;
+                }
+
+                topLeft++;
+                topRight = topLeft + 3;
+            }
+            topLeft = i * 7 + 7;
+            topRight = topLeft + 3;
+        }
+
+        return false;
     }
 });
